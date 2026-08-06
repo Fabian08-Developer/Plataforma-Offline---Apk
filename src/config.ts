4e0f2cbf893e60ea7c59f67b256b2efc7af102e6
fabian_develomet <fabianempresa0@gmail.com>
2026-08-05 21:38:45 -0500

    ok

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const APP_VERSION = "1.1.0";
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = window.location.hostname === 'localhost' ? "http://localhost:3000" : "";
